a1af033966985605b4c8b8efbb444ef99d1f833c
Nelli Gorlova <nelli7035@gmail.com>
2024-09-19 05:35:25 +0600

    added a form and styles for the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-// import { useState } from 'react'
-import './App.css'
+import { useState } from 'react'
+import './App.css';
+import AddTaskForm from './components/AddTaskForm';
 
-function App() {
-
-  return (
+const App = () => (
     <>
-
+        <AddTaskForm/>
     </>
-  )
-}
+);
 
 export default App
